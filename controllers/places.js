eccda1f051fc68f12eaa1d806f3fd4f28c43fc52
Fdavgreen <fdavgreen@gmail.com>
2024-05-08 06:37:12 -0700

    correct?

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,55 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const express = require('express');
+const app = express();
 
 router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+    res.send('places');
+});
 
-module.exports = router
+// GET /places
+router.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }];
+    res.render('places/index', { places });
+});
+
+module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+
+// // GET /places
+// router.get('/', (req, res) => {
+//   // Define the places array
+//   let places = [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: 'http://placekitten.com/250/250'
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: 'http://placekitten.com/250/250'
+//   }];
+
+//   // Render the 'places/index' view template and pass the 'places' array to it
+//   res.render('places/index', { places });
+// });
+
+// module.exports = router;
+
--- index.js
+++ index.js
@@ -2,37 +2,44 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set('view engine','jsx')
+app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.render('Home') // Changed from res.send to res.render
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-  res.render('error404')
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
 
 
-
 // require('dotenv').config()
-// const express = require(`express`)
+// const express = require('express')
 // const app = express()
 
-// app.use(`/places`,require(`./controllers/places`))
+// app.set('view engine','jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+
+// app.use('/places', require('./controllers/places'))
+
+// app.get('/', (req, res) => {
+//   res.render('Home') 
+// })
+
+// app.get('*', (req, res) => {
+//   res.render('error404')
+// })
+
+// app.get('/places', (req, res) => {
+//   res.render('places')
+// })
+
+// app.listen(process.env.PORT)
 
 
 
-// app.get('/', function (req, res) {
-//     res.send('Hello world home page')
-//   })
-
-//   app.get('*', function (req, res) {
-//     res.status(404).send('<h1>404 Page</h1>')
-//   })
-
-//   app.listen(process.env.PORT)
