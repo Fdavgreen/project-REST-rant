3d246bea78f070c258cf3c36b7b29051ecb897a5
Fdavgreen <fdavgreen@gmail.com>
2024-05-24 06:04:12 -0700

    jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -39,29 +39,5 @@ router.get('/', (req, res) => {
 module.exports = router;
 
 
-// const express = require('express');
-// const router = express.Router();
 
-// // GET /places
-// router.get('/', (req, res) => {
-//   // Define the places array
-//   let places = [{
-//     name: 'H-Thai-ML',
-//     city: 'Seattle',
-//     state: 'WA',
-//     cuisines: 'Thai, Pan-Asian',
-//     pic: 'http://placekitten.com/250/250'
-//   }, {
-//     name: 'Coding Cat Cafe',
-//     city: 'Phoenix',
-//     state: 'AZ',
-//     cuisines: 'Coffee, Bakery',
-//     pic: 'http://placekitten.com/250/250'
-//   }];
-
-//   // Render the 'places/index' view template and pass the 'places' array to it
-//   res.render('places/index', { places });
-// });
-
-// module.exports = router;
 
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,20 +6,35 @@ function new_form () {
         <Def>
             <main>
                 <h1>add a new place</h1>
+                <form method="POST" action="/places">
+  <div className="form-group">
+    <label htmlFor="name">Place Name</label>
+    <input className="form-control" id="name" name="name" required />
+  </div>
+  <div className="form-group">
+    <label htmlFor="pic">Place Picture</label>
+    <input className="form-control" id="pic" name="pic" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="city">City</label>
+    <input className="form-control" id="city" name="city" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="state">State</label>
+    <input className="form-control" id="state" name="state" />
+  </div>
+  <div className="form-group">
+    <label htmlFor="cuisines">Cuisines</label>
+    <input className="form-control" id="cuisines" name="cuisines" required />
+  </div>
+  <input className="btn-primary" type="submit" value="Add Place" />
+</form>
+
+
             </main>
         </Def>
     )
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
